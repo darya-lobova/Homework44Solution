75b82a0bfc0fe11893a795831fec690ed7b41059
Darya Lobova <dasha.lobova.it@gmail.com>
2026-03-04 21:35:06 +0300

    Update logic.h

--- Task5C/logic.h
+++ Task5C/logic.h
@@ -1,7 +1,1 @@
-#include <iostream>
-#include <cmath>
-#define DEFAULT_SIZE 100
-using namespace std;
-
-double find_geometric_avg(int array[DEFAULT_SIZE], int length);
-int find_similar_elements(int array[DEFAULT_SIZE], int length, int number);
+//вместо 5с я сделала 2с
